9c427aea664da8ec2dd4375e603933c7a3205872
TipstR <96343885+TipstR@users.noreply.github.com>
2023-06-19 16:25:48 +0200

    changes from 19.06.2023

--- Backend/server.js
+++ Backend/server.js
@@ -60,16 +60,10 @@ try {
     const TOPLEVELPATH = '/api';
     console.log('Binding enpoints, top level Path at ' + TOPLEVELPATH);
 
-    serviceRouter = require('./services/trending.js');
-    app.use(TOPLEVELPATH, serviceRouter);
-
     serviceRouter = require('./services/spiele.js');
     app.use(TOPLEVELPATH, serviceRouter);
 
     serviceRouter = require('./services/benutzer.js');
-    app.use(TOPLEVELPATH, serviceRouter);
-
-    serviceRouter = require('./services/neueSpiele.js');
     app.use(TOPLEVELPATH, serviceRouter);
 
     // send default error message if no matching endpoint found
